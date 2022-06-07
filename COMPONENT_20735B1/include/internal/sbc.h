8e567aa5065534d363d03cae921ae09748dd6765
gitlab-runner <do-not-reply@cypress.com>
2022-06-07 13:51:17 -0500

    Upload 20735B1 3.3.0.23146

--- COMPONENT_20735B1/include/internal/sbc.h
+++ COMPONENT_20735B1/include/internal/sbc.h
@@ -1,7 +1,7 @@
 #ifndef __SBC_H__
 #define __SBC_H__
 /*
- * Copyright 2016-2021, Cypress Semiconductor Corporation (an Infineon company) or
+ * Copyright 2016-2022, Cypress Semiconductor Corporation (an Infineon company) or
  * an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
  *
  * This software, including source code, documentation and related
